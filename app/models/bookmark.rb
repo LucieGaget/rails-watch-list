c05fffb8ee63e33f1414e58e6fcb457bd5c3a8a1
Lucie Gaget <lucie-gaget@orange.fr>
2023-05-25 11:53:12 +0200

    models

---
+++ app/models/bookmark.rb
@@ -0,0 +1,10 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :movie_id, uniqueness: {
+    scope: [ :movie_id, :list_id ]
+  }
+
+  validates :comment, presence: true, length: { minimum: 6 }
+end
